f9dbd031c27542b7c1d4c7e2b687205e8f453fac
Zachary Lake <zlake23@gmail.com>
2020-03-26 20:06:39 -0700

    Removed unneeded script code

--- build.py
+++ build.py
@@ -33,27 +33,4 @@ def main():
         doc = top + content + bottom
         open(page['output'], 'w+').write(doc)
 
-
-    # # Read index file
-    # content = open('./content/index.html').read()
-
-    # # Combine files into new file for about page
-    # index_html = top_html + content + bottom_html
-    # open('./docs/index.html', 'w+').write(index_html)
-
-    # # Combine files into new file for blog page
-    # content = open('./content/blog.html').read()
-    # blog_html = top_html + content + bottom_html
-    # open('./docs/blog.html', 'w+').write(blog_html)
-
-    # # Combine files into new file for projects page
-    # content = open('./content/projects.html').read()
-    # projects_html = top_html + content + bottom_html
-    # open('./docs/projects.html', 'w+').write(projects_html)
-
-    # # Combine files into new file for contact page
-    # content = open('./content/contact.html').read()
-    # contact_html = top_html + content + bottom_html
-    # open('./docs/contact.html', 'w+').write(contact_html)
-
 main()
