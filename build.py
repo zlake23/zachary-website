cae847a77a6a2ec74e032272ed2f511edc1289fa
Zachary Lake <zlake23@gmail.com>
2020-03-30 18:18:24 -0700

    Added templating for active navbar links

--- build.py
+++ build.py
@@ -4,36 +4,37 @@ pages = [
         "filename": "content/index.html",
         "output": "docs/index.html",
         "title": "Zachary Lake",
-        "active": "active",
+        "active": "active-index",
     },
    
     {
         "filename": "content/blog.html",
         "output": "docs/blog.html",
         "title": "My Blog",
-        "active": "active",
+        "active": "active-blog",
     },
 
     {
         "filename": "content/projects.html",
         "output": "docs/projects.html",
         "title": "My Projects",
-        "active": "active",
+        "active": "active-projects",
     },
     {
         "filename": "content/contact.html",
         "output": "docs/contact.html",
         "title": "Contact Page",
-        "active": "active",
+        "active": "active-contact",
     },
 ]
 
 # apply_template function will read in base template and
 # replace content and title string with code from each html page
-def apply_template(content, title):
+def apply_template(content, title, active):
     template = open('./templates/base.html').read()
     template = template.replace("{{content}}", content)
     template = template.replace("{{title}}", title)
+    template = template.replace("{{" + active + "}}", "active")
     return template
 
 # build function will write the completed webpage 
@@ -48,8 +49,9 @@ def main():
     for page in pages:
         content = open(page['filename']).read()
         title = page['title']
+        active = page['active']
         print('Reading:', page['filename'])
-        doc = apply_template(content, title)
+        doc = apply_template(content, title, active)
         print('Compiling template...')
         finished_webpage = build(doc, page)
         print('Writing to docs:', page['output'])
